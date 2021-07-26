62d3a69339faf6202a9415b6c4abd33be666990e
Ari Kauppi <ari.kauppi@santelo.fi>
2021-07-26 03:25:48 +0300

    Alkeellinen laskuri 1. osan c) osuudesta ennen alikomponentteja

--- part1/laskuri/laskuri/src/App.js
+++ part1/laskuri/laskuri/src/App.js
@@ -7,16 +7,26 @@ const App = () => {
   // muuttujiin counter ja setCounter.
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  // Eriytetään nappien tahtumienkäsittelijät omiksi komponentin
+  // sisältäviksi apufunktioiksi.
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const setToZero = () => setCounter(0)
+
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
 
 
   console.log("Rendering... ", counter)
 
   return (
-    <div>{ counter }</div>
+    <div>
+      <div>{ counter }</div>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+    </div>
   )
 }
 
