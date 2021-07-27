36c5550f4539f9c70f53f1ace2ca45e5b3b641f3
Ari Kauppi <ari.kauppi@santelo.fi>
2021-07-27 17:12:36 +0300

    1.11 unicafe step 6 tehty

---
+++ tehtavat/osa1/unicafe_step6/README.md
@@ -0,0 +1,3 @@
+# 1.11 unicafe step 6
+
+Toteuta tilastojen näyttäminen HTML:n taulukkona.
---
+++ tehtavat/osa1/unicafe_step6/src/App.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react'
+
+/*
+Olin itseasiassa jo toteuttanut tämän tehtävän vaatimukset
+pitkälti edellisessä vaiheessa. Muutoksena 1.9 nähden onkin
+lähinnä const Displayn uudelleennimeäminen StatisticsLineksi.
+*/
+
+const StatisticsLine = ({ text, value }) => {
+  // Lisää prosenttimerkin loppuun, jos
+  // ollaan näyttämässä positive-kohtaa
+  if (text === "positive") {
+    return (
+      <tr>
+        <td>
+          {text}
+        </td>
+        <td>
+          {value} %
+        </td>
+      </tr>
+    )
+  }
+
+  return (
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>
+  )
+}
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        give feedback
+      </h1>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const average = (good*1+neutral*0+bad*(-1))/(good+neutral+bad)
+  const positivePortion = (good/(good+neutral+bad)) * 100
+  const all = good+neutral+bad
+
+  // Shows statistics, if any have been added
+  if (all > 0) {
+    return(
+      <table>
+        <StatisticsLine text='good' value={ good } />
+        <StatisticsLine text='neutral' value={ neutral } />
+        <StatisticsLine text='bad' value={ bad } />
+        <StatisticsLine text="all" value={ all } />
+        <StatisticsLine text="average" value={ average } />
+        <StatisticsLine text="positive" value={ positivePortion } />
+      </table>
+      )
+  } else {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick= () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header />
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
