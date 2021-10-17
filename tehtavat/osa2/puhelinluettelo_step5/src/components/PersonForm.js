313aba42d2ef4bdb8e52d657e7c0eb6a4443ed90
Ari Kauppi <ari.kauppi@santelo.fi>
2021-10-17 04:39:36 +0300

    PersonFormin alku

---
+++ tehtavat/osa2/puhelinluettelo_step5/src/components/PersonForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+<form onSubmit={addPerson}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          phone: <input 
+            value={newPhone}
+            onChange={handlePhoneChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+</form>
